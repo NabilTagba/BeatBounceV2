386434f90adb544b6fa4f4b73dd55f52e2fb4fcc
rswansonbradley <raswanson@mail.bradley.edu>
2023-03-04 22:02:07 -0600

    GameStateUpdates

--- Assets/scripts/RoundHandler.cs
+++ Assets/scripts/RoundHandler.cs
@@ -5,12 +5,34 @@ using UnityEngine;
 public class RoundHandler : MonoBehaviour
 {
     public GameObject p1, p2;
-    public Transform p1StartLoc, p2StartLoc;
+    public Vector3 p1StartLoc, p2StartLoc;
     public int p1Score, p2Score = 0;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    private void Update()
+    {
+        //THIS IS PURELY FOR TESTING REMOVE THIS LATER
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            ResetGameState();
+        }
+
+        if(p1 == null || p2 == null)
+        {
+            GameObject tempPlayer = GameObject.FindGameObjectWithTag("Player");
+            if(p1 == null)
+            {
+                p1 = tempPlayer;
+            }
+            else if (p2 == null && tempPlayer != p1)
+            {
+                p2 = tempPlayer;
+            }
+        }
     }
 
     public void UpdateScore(bool whichPlayer)
@@ -35,8 +57,8 @@ public class RoundHandler : MonoBehaviour
         p2.GetComponent<PlayerMovement>().StopPlayerMovement();
         p1.GetComponent<Rigidbody>().velocity = Vector3.zero;
         p2.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        p1.transform.position = p1StartLoc.transform.position;
-        p2.transform.position = p2StartLoc.transform.position;
+        p1.transform.position = p1StartLoc;
+        p2.transform.position = p2StartLoc;
     }
 
     void CheckForWin()
